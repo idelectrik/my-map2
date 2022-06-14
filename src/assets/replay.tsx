01a49611acd662d7c1f62f92085ae4c7a4a83c49
idelectrik <idelectrik@gmail.com>
2022-06-14 09:32:12 +0200

    Add files via upload

---
+++ src/assets/replay.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="replay" tilewidth="32" tileheight="32" tilecount="320" columns="20">
+ <image source="../../../replay.png" trans="172566" width="640" height="512"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="4" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="6" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="7" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="24" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="25" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="300"/>
+   <frame tileid="26" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="300"/>
+   <frame tileid="27" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
