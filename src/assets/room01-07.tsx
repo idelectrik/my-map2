0e221febd4b3c6e393b237d70f0deecd8ba807ce
idelectrik <idelectrik@gmail.com>
2022-06-14 09:35:13 +0200

    Add files via upload

---
+++ src/assets/room01-07.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="room01-07" tilewidth="32" tileheight="32" tilecount="320" columns="20">
+ <image source="../../../room01-07.png" trans="172566" width="640" height="512"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="7" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="23">
+  <animation>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="27" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ src/assets/room02-07.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="room02-07" tilewidth="32" tileheight="32" tilecount="320" columns="20">
+ <image source="../../../room02-07.png" trans="172566" width="640" height="512"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="5" duration="900"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="21" duration="200"/>
+   <frame tileid="25" duration="900"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="25" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
